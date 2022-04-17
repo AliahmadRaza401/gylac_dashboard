88cb3150ff4b48c879a43a6dc95d1b0a67bb080e
unknown <masoomraza752@gmail.com>
2022-04-17 21:38:23 +0500

    sunday

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'Screens/home_page.dart';
+import 'package:gylac_dashboard/Screens/login_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage()
+      home: LoginScreen()
     );
     });
   }
